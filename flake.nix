ed69502f4a1370ccefed0bcedf12e566621ee0f6
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-06-12 20:09:40 +0800

    feat(home): add home-manager

--- flake.nix
+++ flake.nix
@@ -2,24 +2,42 @@
   description = "Nix Flakes Configuration of Enkaiyuegure";
 
   outputs = inputs @ { self, ... }: {
-    nixosConfigurations."ltp-zbook-nix" = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        inputs.disko.nixosModules.disko
-        inputs.impermanence.nixosModules.impermanence
+    nixosConfigurations = {
+      "ltp-zbook-nix" = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          inputs.disko.nixosModules.disko
+          inputs.impermanence.nixosModules.impermanence
 
-        ./modules/hosts/dae.nix
-        ./lib/disko_layout/ltp-zbook-nix.nix
-        ./modules/hosts/impermanence.nix
+          ./modules/hosts/dae.nix
+          ./lib/disko_layout/ltp-zbook-nix.nix
+          ./modules/hosts/impermanence.nix
 
-        ./hosts/ltp/zbook
-      ];
+          ./hosts/ltp/zbook
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager = {
+	      useGlobalPkgs = true;
+	      useUserPackages = true;
+	      extraSpecialArgs = inputs;
+	      users.enkai = import ./home;
+	    };
+	  }
+        ];
+      };
     };
   };
 
   inputs = {
     #-- nixpkgs -- Nix packages collection & NixOS
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #-- home-manager -- Manage a user environment using Nix
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     #-- disko -- Declarative disk partitioning and formatting using nix
     disko.url = "github:nix-community/disko";
     #-- impermanence -- Modules to help you handle persistent state on systems with ephemeral root storage
