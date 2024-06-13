d0427125847b6466b7bf129637c9c7d5b911fad1
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-06-13 16:21:34 +0800

    fix: fix broken defaultSession setting in displayManager

--- modules/i3.nix
+++ modules/i3.nix
@@ -5,45 +5,50 @@
 
   # i3 related options
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
-  services.xserver = {
-    enable = true;
+  services = {
+    xserver = {
+      enable = true;
 
-    desktopManager = {
-      xterm.enable = false;
-    };
+      desktopManager = {
+        xterm.enable = false;
+      };
    
-    displayManager = {
-        defaultSession = "none+i3";
+      displayManager = {
         lightdm.enable = false;
         gdm.enable = true;
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          rofi          # application launcher, the same as dmenu
+          dunst         # notification daemon
+          i3blocks      # status bar
+          i3lock        # default i3 screen locker
+          xautolock     # lock screen after some time
+          i3status      # provide information to i3bar
+          i3-gaps       # i3 with gaps
+          picom         # transparency and shadows
+          feh           # set wallpaper
+          acpi          # battery information
+          arandr        # screen layout manager
+          dex           # autostart applications
+          xbindkeys     # bind keys to commands
+          xorg.xbacklight  # control screen brightness
+          xorg.xdpyinfo      # get screen information
+          sysstat       # get system information
+       ];
+      };
+
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
     };
 
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        rofi          # application launcher, the same as dmenu
-        dunst         # notification daemon
-        i3blocks      # status bar
-        i3lock        # default i3 screen locker
-        xautolock     # lock screen after some time
-        i3status      # provide information to i3bar
-        i3-gaps       # i3 with gaps
-        picom         # transparency and shadows
-        feh           # set wallpaper
-        acpi          # battery information
-        arandr        # screen layout manager
-        dex           # autostart applications
-        xbindkeys     # bind keys to commands
-        xorg.xbacklight  # control screen brightness
-        xorg.xdpyinfo      # get screen information
-        sysstat       # get system information
-     ];
-    };
-
-    # Configure keymap in X11
-    xkb = {
-      layout = "us";
-      variant = "";
+    displayManager = {
+      defaultSession = "none+i3";
     };
   };
 
