4ad11628eea803223f645459afa7579fdbac6acf
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-06-14 08:09:13 +0800

    refactor(browsers): rm chromium

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -4,17 +4,9 @@
   ...
 }: {
   programs = {
-    chromium = {
-      enable = true;
-      commandLineArgs = ["--enable-features=TouchpadOverscrollHistoryNavigation"];
-      extensions = [
-        # {id = "";}  // extension id, query from chrome web store
-      ];
-    };
-
     firefox = {
       enable = true;
-      profiles.ryan = {};
+      profiles.enkai = {};
     };
   };
 }
