26c8356423db6d2dbdc4aaef7ab693a59da10a69
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-06-18 13:32:21 +0800

    refactor(i3): mv directory home/i3 to home/wm/i3

--- home/wm/i3/default.nix
+++ home/wm/i3/default.nix
@@ -5,7 +5,7 @@
 }:
 {
   home.file = {
-    ".config/i3/wallpaper.jpg".source = ../../wallpaper.jpg;
+    ".config/i3/wallpaper.jpg".source = ../../../wallpaper.jpg;
     ".config/i3/config".source = ./config;
     ".config/i3/i3blocks.conf".source = ./i3blocks.conf;
     ".config/i3/keybindings".source = ./keybindings;
