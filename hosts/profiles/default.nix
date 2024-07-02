f53ca91e23da2bd6f6baf54e3813d2758a54e33c
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-02 14:34:43 +0800

    test(flake-parts): test simplify my configuration with flake-parts

---
+++ hosts/profiles/default.nix
@@ -0,0 +1,34 @@
+{ inputs, ... }:
+{
+  flake.nixosConfigurations = {
+    "ltp-zbook-nix" = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        inputs.disko.nixosModules.disko
+        inputs.impermanence.nixosModules.impermanence
+
+        ../../modules/hosts/dae.nix
+        ../../lib/disko_layout/ltp-zbook-nix.nix
+        ../../modules/hosts/impermanence.nix
+
+        ../ltp/zbook
+
+	inputs.home-manager.nixosModules.home-manager
+	{
+	  home-manager = {
+	    useGlobalPkgs = true;
+	    useUserPackages = true;
+	    extraSpecialArgs = inputs;
+	    users.enkai = import ./home;
+	  };
+	}
+      ];
+    };
+  };
+  "tower-qtj1-nix" = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+
+    ];
+  };
+}
