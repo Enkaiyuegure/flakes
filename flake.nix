3b2fa82cd76dfed3060da3f813e2bd723f4d8bcb
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-02 14:34:43 +0800

    test(flake-parts): test simplify my configuration with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,39 +1,21 @@
 {
   description = "Nix Flakes Configuration of Enkaiyuegure";
 
-  outputs = inputs @ { self, ... }: {
-    nixosConfigurations = {
-      "ltp-zbook-nix" = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          inputs.disko.nixosModules.disko
-          inputs.impermanence.nixosModules.impermanence
+  outputs = inputs @ { self, ... }: 
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        
+      };
+      imports = [
+        ./hosts/profiles
+      ];
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, ... }: {
 
-          ./modules/hosts/dae.nix
-          ./lib/disko_layout/ltp-zbook-nix.nix
-          ./modules/hosts/impermanence.nix
-
-          ./hosts/ltp/zbook
-
-	  inputs.home-manager.nixosModules.home-manager
-	  {
-	    home-manager = {
-	      useGlobalPkgs = true;
-	      useUserPackages = true;
-	      extraSpecialArgs = inputs;
-	      users.enkai = import ./home;
-	    };
-	  }
-        ];
       };
     };
-    "tower-qtj1-nix" = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-
-      ];
-    };
-  };
 
   inputs = {
     #-- nixpkgs -- Nix packages collection & NixOS
