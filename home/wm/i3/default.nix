868c47e9ae41e673ac3876f7532f9f0b25e18d4e
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-14 15:10:59 +0800

    feat(i3/wall): Adjust the wallpaper settings under i3

--- home/wm/i3/default.nix
+++ home/wm/i3/default.nix
@@ -1,11 +1,9 @@
-{
-  pkgs,
-  config,
-  ...
+{ pkgs
+, config
+, ...
 }:
 {
   home.file = {
-    ".config/i3/wallpaper.jpg".source = ../../../wallpaper.jpg;
     ".config/i3/config".source = ./config;
     ".config/i3/i3blocks.conf".source = ./i3blocks.conf;
     ".config/i3/keybindings".source = ./keybindings;
