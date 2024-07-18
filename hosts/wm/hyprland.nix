50db694e943aeb95b6dcdc0795f8c400a1ad54a7
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-18 11:29:05 +0800

    feat(host/wm): Add Hyprland

---
+++ hosts/wm/hyprland.nix
@@ -0,0 +1,15 @@
+{ pkgs, inputs, ... }:
+{
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+}
