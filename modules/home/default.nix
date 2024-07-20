017b02409a34a50392f8664730a1fbda18a128c5
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-20 22:49:27 +0800

    feat(home): Update to 24.11 version

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -5,7 +5,7 @@
     homeDirectory = "/home/${userName}";
     language.base = "en_US.UTF-8";
 
-    stateVersion = "24.05";
+    stateVersion = "24.11";
   };
 
   programs.home-manager.enable = true;
