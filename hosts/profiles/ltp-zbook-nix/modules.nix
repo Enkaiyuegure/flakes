ec3ab428eef987ee167edbe269bd6052053739ac
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-22 15:14:22 +0800

    feat(host): Move relative modules to profiles/ltp-zbook-nix/modules.nix

--- hosts/profiles/ltp-zbook-nix/modules.nix
+++ hosts/profiles/ltp-zbook-nix/modules.nix
@@ -1,5 +1,11 @@
 {
   imports = [
     ../../wm/i3.nix
+
+    ../../../modules/hosts/dae.nix
+    ../../../lib/disko_layout/ltp-zbook-nix.nix
+    ../../../modules/hosts/impermanence.nix
+
+
   ];
 }
