10d56d9ba780f227253e6552b5aaca1d46a4a3c1
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-22 15:42:28 +0800

    feat: Remove i3bar & i3blocks

--- home/wm/i3/default.nix
+++ home/wm/i3/default.nix
@@ -5,7 +5,6 @@
 {
   home.file = {
     ".config/i3/config".source = ./config;
-    ".config/i3/i3blocks.conf".source = ./i3blocks.conf;
     ".config/i3/keybindings".source = ./keybindings;
     ".config/i3/scripts" = {
       source = ./scripts;
