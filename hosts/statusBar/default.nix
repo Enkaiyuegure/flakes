3e8c5889d5a9f7724e68883994c3f0f75b67440a
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-07-25 18:38:34 +0800

    feat(home/statusBar): Modify polybar in hosts/wm/i3.nix to polybarFull in hosts/statusBar/polybar.nix & Add i3 module Display information about workspaces and active mode

--- hosts/statusBar/default.nix
+++ hosts/statusBar/default.nix
@@ -1,5 +1,5 @@
 {
   imports = [
-    ./polybar
+    ./polybar.nix
   ];
 }
--- hosts/statusBar/polybar.nix
+++ hosts/statusBar/polybar.nix
@@ -1,3 +1,6 @@
+{ pkgs, ... }:
 {
-  programs.polybar.enable = true;
+  environment.systemPackages = with pkgs; [
+    polybarFull
+  ];
 }
