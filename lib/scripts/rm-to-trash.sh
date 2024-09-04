bc26d705dd8bfde08a16bbf3be83fc3e0ab858f5
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-09-04 10:01:01 +0800

    feat(scripts): Modify the path of .trash directory

--- lib/scripts/rm-to-trash.sh
+++ lib/scripts/rm-to-trash.sh
@@ -1,13 +1,13 @@
-dest_dir="/.trash"
+dest_dir="/home/enkai/.trash"
 timestamp=$(date +"%Y%m%d_%H%M%S")
 
 for file in "$@"; do
-  if [ -e "$file" ]; then
-    filename=$(basename "$file")
-    new_name="${filename}_$timestamp"
-    mv "$file" "$dest_dir/$new_name" 
-    echo "Moved $file to $dest_dir/$new_name."
-  else
-    echo "File $file not found."
-  fi
+	if [ -e "$file" ]; then
+		filename=$(basename "$file")
+		new_name="${filename}_$timestamp"
+		mv "$file" "$dest_dir/$new_name"
+		echo "Moved $file to $dest_dir/$new_name."
+	else
+		echo "File $file not found."
+	fi
 done
