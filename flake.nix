cc41f826c97eca97eaa047a39ffc90d033e6ee03
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-09-04 09:51:50 +0800

    feat: Add some inputs link and improve the comment in flake.nix & update flake.lock

--- flake.nix
+++ flake.nix
@@ -42,57 +42,95 @@
   };
 
   inputs = {
-    #-- nixpkgs -- Nix packages collection & NixOS
+    ##########################################systemBase###########################################
+    #================================Nix Packages collection & NixOS==============================#
+    #-----------Official NixOS package source, using nixos's unstable branch by default-----------#
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #-- nixpkgs-stable -- Nix stable packages
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    #-- nixpkgs-unstable -- Nix unstable packages
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
-
-    #-- home-manager -- Manage a user environment using Nix
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    #=============================Manage a user environment using Nix=============================#
+    #---------------------home-manager, used for managing user configuration----------------------#
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    #-- flake-parts -- Simplify Nix Flakes with the module system
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    #-- flake-root -- A `flake-parts` module for finding your way to the project root directory
-    flake-root.url = "github:srid/flake-root";
-
-    anyrun = {
-      url = "github:Kirottu/anyrun";
+    #===================================Secure Boot for NixOS=====================================#
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    #-- disko -- Declarative disk partitioning and formatting using nix
-    disko.url = "github:nix-community/disko";
-    #-- impermanence -- Modules to help you handle persistent state on systems with ephemeral root storage
+    #===================Declarative disk partitioning and formatting using nix====================#
+    disko = {
+      url = "github:nix-community/disko/v1.6.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #======Modules to help you handle persistent state on systems with ephemeral root storage=====#
     impermanence.url = "github:nix-community/impermanence";
-    flake-registry = {
-      url = "github:NixOS/flake-registry";
+    ######################################desktopEnvironment#######################################
+    #=============================A customizable and extensible shell=============================#
+    ags.url = "github:Aylur/ags";
+    #=============================A material you color generation tool============================#
+    matugen.url = "github:InioX/matugen?ref=v2.2.0";
+    #==========JS/TS framework for creating Linux Desktops ontop of Wayland compositors===========#
+    astal.url = "github:Aylur/astal";
+    #=========================================myWallpapers========================================#
+    wallpapers = {
+      url = "github:Enkaiyuegure/wallpapers";
       flake = false;
     };
+    #########################################systemTools###########################################
+    #=================================Collection of image builders================================#
+    #----------------generate iso/qcow2/docker/... image from nixos configuration-----------------#
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #===============age-encrypted secrets for NixOS; drop-in replacement for agenix===============#
+    agenix = {
+      url = "github:yaxitech/ragenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #==============================The Proxmox Hypervisor, on NixOS#==============================#
+    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";
+    #######################################othersNixProject########################################
+    #=========================================Gaming on Nix=======================================#
+    nix-gaming.url = "github:fufexan/nix-gaming";
+    #======================Nix User Repository: User contributed nix packages=====================#
     nur.url = "github:nix-community/NUR";
+    #==============================Weekly updated nix-index database==============================#
     nix-index-database = {
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-
-    matugen.url = "github:InioX/matugen?ref=v2.2.0";
-    ags.url = "github:Aylur/ags";
-    astal.url = "github:Aylur/astal";
-
-    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";
-
-    nix-gaming.url = "github:fufexan/nix-gaming";
-
-    # Add git hooks to format nix code before commit
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    nvim-flake.url = "github:Ruixi-rebirth/nvim-flake";
+    #=============Seamless integration of https://pre-commit.com git hooks with Nix.==============#
+    #------------------------add git hooks to format nix code before commit-----------------------#
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #============================Filesystem-based module system for Nix===========================#
+    haumea = {
+      url = "github:nix-community/haumea/v0.2.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #=========================#Nix language server, based on nix libraries========================#
     nixd.url = "github:nix-community/nixd";
+    #===================================treefmt nix configuration=================================#
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    #############################################end###############################################
+    #-- flake-parts -- Simplify Nix Flakes with the module system
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    #-- flake-root -- A `flake-parts` module for finding your way to the project root directory
+    flake-root.url = "github:srid/flake-root";
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-registry = {
+      url = "github:NixOS/flake-registry";
+      flake = false;
+    };
+    nvim-flake.url = "github:Ruixi-rebirth/nvim-flake";
   };
 
   nixConfig = {
