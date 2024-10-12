64802346c7d8ae1dc87b5367d0bbf0f427ccd542
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-10-12 21:39:40 +0800

    feat: Remove unnecessary nix settings in old-hosts/profiles/ltp-zbook-nix/default.nix

--- old-hosts/profiles/ltp-zbook-nix/default.nix
+++ old-hosts/profiles/ltp-zbook-nix/default.nix
@@ -173,25 +173,6 @@
   #   enableSSHSupport = true;
   # };
 
-  nix = {
-    channel.enable = false;
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      trusted-users = [ "root" "@wheel" ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 2d";
-    };
-  };
-
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   # List services that you want to enable:
