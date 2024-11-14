d63a64f8273ec5019ecb6f37a04878dbc966a8a7
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-11-14 19:15:53 +0800

    fix(obsidian): Fix the input method issue within `obsidian`
    Resolve: Add a command-line argument to fix it, more is referenced in the comments within related line

--- home/linux/gui/base/note-taking.nix
+++ home/linux/gui/base/note-taking.nix
@@ -1,6 +1,10 @@
-{pkgs-stable, ...}: {
-  home.packages = with pkgs-stable; [
-    obsidian
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # from https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
+    (obsidian.override {
+      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime";
+    })
+    obsidian-export
     zotero
   ];
 }
