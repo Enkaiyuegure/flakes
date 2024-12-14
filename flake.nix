5de9ade3ad2312c2917b142b5d3b63807d0281bb
Enkaiyuegure <enkaiyuegure@outlook.com>
2024-12-14 12:51:47 +0800

    feat: Update Nix version 24.05 to 24.11

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     #-----------Official NixOS package source, using nixos's unstable branch by default-----------#
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     #===========================================nixlib============================================#
     nixlib.url = "github:nix-community/nixpkgs.lib";
     #================A modern, delicious implementation of the Nix package manager================#
