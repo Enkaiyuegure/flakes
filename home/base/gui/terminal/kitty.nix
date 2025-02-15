56612b0859ef9dc004bbcf0f0e3756f0e1d7230d
Enkaiyuegure <enkaiyuegure@outlook.com>
2025-02-15 17:53:20 +0800

    feat: Add the cursor animation for kitty

--- home/base/gui/terminal/kitty.nix
+++ home/base/gui/terminal/kitty.nix
@@ -39,6 +39,7 @@
       background_opacity = "0.93";
       macos_option_as_alt = true; # Option key acts as Alt on macOS
       enable_audio_bell = false;
+      cursor_trail = "3";
       tab_bar_edge = "top"; # tab bar on top
       #  To resolve issues:
       #    1. https://github.com/ryan4yin/nix-config/issues/26
