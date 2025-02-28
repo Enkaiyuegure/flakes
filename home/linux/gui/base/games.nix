cdea7bf5b75c51899290e3620bb522de5d4aceb7
Enkaiyuegure <enkaiyuegure@outlook.com>
2025-02-28 10:58:16 +0800

    feat: Add Ghidra and Okteta

--- home/linux/gui/base/games.nix
+++ home/linux/gui/base/games.nix
@@ -8,5 +8,8 @@
     # gamescope # SteamOS session compositing window manager
     prismlauncher # A free, open source launcher for Minecraft
     winetricks # A script to install DLLs needed to work around problems in Wine
+
+    okteta # A hex editor
+    ghidra # A software reverse engineering (SRE) suite of tools 
   ];
 }
