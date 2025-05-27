0c5a7ee3e7030b76e0c97d94d00b62786a47aef0
Enkaiyuegure <enkaiyuegure@outlook.com>
2025-05-27 11:18:48 +0800

    fix(hyprland): fix the issue 'Hyprland stutters a lot after an upgrade to Linux 6.11, adds an invisible `unknown` monitor between the two.' ref hyprwm/Hyprland#7971

--- home/linux/gui/hyprland/options/nvidia.nix
+++ home/linux/gui/hyprland/options/nvidia.nix
@@ -16,6 +16,8 @@ in {
       "LIBVA_DRIVER_NAME,nvidia"
       "XDG_SESSION_TYPE,wayland"
       "GBM_BACKEND,nvidia-drm"
+      # issue https://github.com/hyprwm/Hyprland/issues/7971
+      "AQ_DRM_DEVICES,/dev/dri/card1"
       "__GLX_VENDOR_LIBRARY_NAME,nvidia"
       # fix https://github.com/hyprwm/Hyprland/issues/1520
       "WLR_NO_HARDWARE_CURSORS,1"
